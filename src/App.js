da46d04c7fc5251f6b52146eaeebca4a1525439a
Abhishek <choudharyabhishekk@gmail.com>
2024-09-09 18:38:00 -0400

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
-import BookingForm from "./Components/BookingForm";
-import Navbar from "./Components/Navbar";
-import { useReducer, useState } from "react";
-const initializeTimes = () => {
-  return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
-};
+import React from 'react';
+import './App.css';
+import Nav from "./components/Nav"
+import Header from "./components/Header"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
 
-const updateTimes = (state, action) => {
-  switch (action.type) {
-    case "UPDATE_TIMES":
-      return state;
-    default:
-      return state;
-  }
-};
 function App() {
-  const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes());
-
   return (
+
     <>
-      <Navbar />
-      <BookingForm availableTimes={availableTimes} dispatch={dispatch} />
+      <Nav />
+      <Main />
+      <Footer />
     </>
   );
 }
